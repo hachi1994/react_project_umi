76522d8a30003bc699f64db9635fe189e338289f
hachi1994 <lhc1092751647@163.com>
2021-12-09 18:05:12 +0800

    #2021-12-09# react_umi 项目创建

---
+++ .umirc.ts
@@ -0,0 +1,41 @@
+import { IConfig } from 'umi-types';
+// ref: https://umijs.org/config/
+const config: IConfig = {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
+  proxy: {
+    "/api": {
+      target: 'http://api.jaelizumi.cn/',
+      "changeOrigin": true
+    }
+  },
+  chainWebpack(config,{webpack}){
+    config.resolve.alias.set('s','/src')
+  },
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'react_umi',
+      dll: false,
+
+      routes: {
+        exclude: [
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import styles from './index.css';
+import { Button, message,Upload } from 'antd'
+const props = {
+  name: 'file',
+  action: '/api/uploadImg',
+  // headers: {
+  //     authorization: 'authorization-text',
+  // },
+  onChange(info: any) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+function App() {
+  let [count, setCount] = useState(1)
+  return (
+    <div className={styles.normal}>
+      <Button type='primary' onClick={() => setCount(() => count++)}>{count}</Button>
+      <Upload {...props}>
+        <Button type='primary'>Click to Upload</Button>
+      </Upload>
+    </div>
+  )
+}
+export default App
