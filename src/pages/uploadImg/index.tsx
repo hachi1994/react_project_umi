68378dc08de571d044275d39cbd6a0d2ebba7e9f
hachi1994 <lhc1092751647@163.com>
2021-12-13 23:08:21 +0800

    #2021-12-13_1# react_umi model

--- src/pages/uploadImg/index.tsx
+++ src/pages/uploadImg/index.tsx
@@ -1,38 +1,52 @@
 import React, { useState } from 'react';
 import { Button, message, Upload } from 'antd'
 import { connect } from 'dva'
-import styles from './index.less' 
-const props = {
-    name: 'file',
-    action: '/api/uploadImg',
-    // headers: {
-    //     authorization: 'authorization-text',
-    // },
-    onChange(info: any) {
-        if (info.file.status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (info.file.status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-};
+import styles from './index.less'
+import axios from 'axios'
 function App(props: any) {
-    console.log(props, 441)
-    function foc(){
+    console.log(props.model)
+    let [count, setCount] = useState(1)
+    const uploadProps = {
+        name: 'file',
+        action: '/api/uploadImg',
+        // headers: {
+        //     authorization: 'authorization-text',
+        // },
+        onChange(info: any) {
+            if (info.file.status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (info.file.status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully`);
+            } else if (info.file.status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+    };
+    async function foc() {
+        let res = await axios.get('/api/getJsonData')
+        if (res.status === 200) {
+            setCount(res.data.name)
+        }
         props.dispatch({
-            type:'model/setState'
+            type:'model/getName',
+            payload:{
+                name:'13'
+            }
+        })
+        props.dispatch({
+            type: 'model/setState',
+            payload:{
+                id:'lhc'
+            }
         })
     }
-    let [count, setCount] = useState(1)
-    const {qq='wade'} = props.model
+    const { qq = 'wade' } = props.model
     return (
         <div className={styles.normal}>
             {qq}
             <Button type='primary' onClick={foc}>{count}</Button>
-            <Upload {...props}>
+            <Upload {...uploadProps}>
                 <Button type='primary'>Click to Upload</Button>
             </Upload>
         </div>
@@ -45,4 +59,4 @@ function App(props: any) {
 //     return {...uploadImg}
 // })(App)
 
-export default connect((uploadImg) => {return {...uploadImg}})(App)
+export default connect((uploadImg) => { return { ...uploadImg } })(App)
--- src/pages/uploadImg/model.js
+++ src/pages/uploadImg/model.js
@@ -1,19 +1,25 @@
+import axios from "axios";
 export default {
-    //namespace: 'uploadImg',
-    state: {
-        name:1,
-        age:20
-    },
+  //namespace: 'uploadImg',
+  state: {
+    name: 1,
+    age: 20
+  },
+  effects: {
+    *getName(payload, { call, put }) {
+      let res = yield axios.get('/api/getName')
+      yield put({ type: 'setState', payload: { id: res.data.msg } })
+    }
+  },
+  reducers: {
+    'setState'(state, { payload: { id } }) {
+      let qq = id
+      let o = {
+        ...state,
+        qq
+      };
 
-    reducers: {
-      'setState'(state, { payload: id }) {
-        let qq = 'lhc'
-        let o = {
-          ...state,
-          qq
-        };
-
-        return o
-      },
+      return o
     },
-  };
+  },
+};
